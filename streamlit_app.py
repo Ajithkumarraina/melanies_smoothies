dea632777930bec0ae46c547974f7660b2e3b387
Ajithkumarraina <165248528+Ajithkumarraina@users.noreply.github.com>
2024-04-01 13:33:03 +0530

    Update streamlit_app.py
    Added

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,16 +8,14 @@ import requests
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie :cup_with_straw:")
 st.write(""" Choose the fruit you want in your custom smoothie! """)
-#my_dataframe=session.table("smoothies.public.tables").filter(col("Order_feild"))
-#editable_df= st.experimental_data_editor(my_dataframe)
 
 name_on_order= st.text_input('Name on Smoothie')
 st.write('The name on your smoothie will be:', name_on_order)
 
 cnx = st.connection("snowflake")
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('fruit_name'),col('search_on'))
-st.dataframe(data=my_dataframe, use_container_width=True)
+#my_dataframe = session.table("smoothies.public.fruit_options").select(col('fruit_name'),col('search_on'))
+#st.dataframe(data=my_dataframe, use_container_width=True)
 #st.stop()
 
 pd_df=my_dataframe.to_pandas()
